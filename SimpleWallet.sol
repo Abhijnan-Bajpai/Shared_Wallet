aac0c0d7fa6856ac3feb9ae7ea0ac8b388dee6a8
Abhijnan-Bajpai <abhijnanbajpai@outlook.com>
2021-07-31 11:58:42 +0530

    Securing transactions by creating an owner level privilege

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -4,7 +4,15 @@ pragma solidity 0.8.1;
 
 contract SimpleWallet {
     
-    function withdrawMoney(address payable _to, uint _amount) public {
+    address owner;
+    
+    // A modifier which ensures the security of certain transactions by limiting their access to the owner
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You don't have the required privileges");
+        _;
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
     
