9868931a82fb0c6f3c57f229b69fff4cad283a1b
Abhijnan-Bajpai <abhijnanbajpai@outlook.com>
2021-07-31 11:47:12 +0530

    The basic transaction

---
+++ SimpleWallet.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.1;
+
+contract SimpleWallet {
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        _to.transfer(_amount);
+    }
+    
+    receive() external payable {
+        
+    }
+    
+}
