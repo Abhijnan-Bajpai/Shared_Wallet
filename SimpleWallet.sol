760f3009343ee2c85ee1dab409d8dadc3bf0009b
Abhijnan-Bajpai <abhijnanbajpai@outlook.com>
2021-08-01 08:04:48 +0530

    Imported ownable.sol from openzepplin contracts for better security

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -2,15 +2,20 @@
 
 pragma solidity 0.8.1;
 
-contract SimpleWallet {
+//OpenZeppelin contract for better security
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SimpleWallet is Ownable {
     
-    address owner;
+    // address owner;
+    
     
     // A modifier which ensures the security of certain transactions by limiting their access to the owner
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You don't have the required privileges");
-        _;
-    }
+    // Not required after import of Ownable.sol
+    // modifier onlyOwner() {
+    //     require(msg.sender == owner, "You don't have the required privileges");
+    //     _;
+    // }
     
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
